376dfb480c775c806d88c81f01ccc93718d7251e
Nils Haberkamp <haberkamp.n@gmail.com>
2025-07-25 23:55:04 +0200

    test: focuses the last item when pressing the left arrow key on the first element

--- src/index.browser.test.tsx
+++ src/index.browser.test.tsx
@@ -179,3 +179,27 @@ it("focues the previous item when pressing the left arrow key", async () => {
     .element(screen.getByText("Item 3"))
     .toHaveAttribute("tabindex", "-1");
 });
+
+it("focuses the last item when pressing the left arrow key on the first item", async () => {
+  // ARRANGE
+  const screen = await render(
+    <RovingIndexGroup>
+      <RovingIndexItem>Item 1</RovingIndexItem>
+      <RovingIndexItem>Item 2</RovingIndexItem>
+    </RovingIndexGroup>,
+  );
+
+  await userEvent.tab();
+  await userEvent.keyboard("{ArrowLeft}");
+
+  // ASSERT
+  await expect.element(screen.getByText("Item 2")).toHaveFocus();
+  await expect
+    .element(screen.getByText("Item 2"))
+    .toHaveAttribute("tabindex", "0");
+
+  await expect.element(screen.getByText("Item 1")).not.toHaveFocus();
+  await expect
+    .element(screen.getByText("Item 1"))
+    .toHaveAttribute("tabindex", "-1");
+});
