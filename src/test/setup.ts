09b2c83d4a09430c1458e26dc0278992baeadd71
Nils Haberkamp <haberkamp.n@gmail.com>
2025-07-25 20:43:47 +0200

    build: fix vitest setup

--- src/test/setup.ts
+++
@@ -1,9 +0,0 @@
-// Test setup file for Vitest
-import { afterEach } from "vitest";
-import { cleanup } from "@testing-library/react";
-import "@testing-library/jest-dom";
-
-// Cleanup after each test case (e.g. clearing jsdom)
-afterEach(() => {
-  cleanup();
-});
