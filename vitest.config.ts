74d49602937e147ab53bcb40f4e3ae663aeb8370
Nils Haberkamp <haberkamp.n@gmail.com>
2025-12-09 19:28:26 +0100

    test: add firefox and webkit for testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -15,7 +15,11 @@ export default defineConfig({
           browser: {
             enabled: true,
             provider: "playwright",
-            instances: [{ browser: "chromium" }],
+            instances: [
+              { browser: "chromium" },
+              { browser: "firefox" },
+              { browser: "webkit" },
+            ],
           },
         },
       },
